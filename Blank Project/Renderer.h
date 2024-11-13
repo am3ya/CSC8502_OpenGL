4bf545d7c86b1bebc6dd5d1d5cbc945b1c4c1157
am3ya <142191552+am3ya@users.noreply.github.com>
2024-11-13 14:29:29 +0000

    Add files via upload

--- Blank Project/Renderer.h
+++ Blank Project/Renderer.h
@@ -1,10 +1,15 @@
 #pragma once
 
 #include"../nclgl/OGLRenderer.h"
+#include"../nclgl/SceneNode.h"
+#include"../nclgl/Frustum.h"
 
 class Camera;
 class Shader;
 class HeightMap;
+class MeshMaterial;
+class SceneNode;
+class MeshAnimation;
 
 class Renderer : public OGLRenderer {
 public:
@@ -28,6 +33,17 @@ protected:
 	Light* light;
 	Camera* camera;
 
+	void BuildNodeLists(SceneNode* from);
+	void SortNodeLists();
+	void ClearNodeLists();
+	void DrawNodes();
+	void DrawNode(SceneNode* n);
+	SceneNode* root;
+	Frustum frameFrustum;
+	vector<SceneNode*> transparentNodeList;
+	vector<SceneNode*> nodeList;
+
+
 	GLuint cubeMap;
 	GLuint waterTex;
 	GLuint earthTex;
@@ -36,4 +52,21 @@ protected:
 
 	float waterRotate;
 	float waterCycle;
+
+	Mesh* treeMesh;
+	Shader* meshShader;
+	Shader* animMeshShader;
+	MeshAnimation* treeAnim;
+	MeshMaterial* treeMaterial;
+	vector<GLuint> treeTextures;
+
+	void DrawPostProcess();
+	Shader* processShader;
+	Shader* sceneShader;
+	GLuint bufferFBO;
+	GLuint processFBO;
+	GLuint bufferColourTex[2];
+	GLuint bufferDepthTex;
+
+	bool postProcess;
 };
